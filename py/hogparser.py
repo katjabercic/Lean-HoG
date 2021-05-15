c2212039fb936ff9ace206a060c0e6f4dc72d866
Katja Berčič <katja.bercic@gmail.com>
2021-05-15 12:16:25 +0200

    parse hog export to lean database (!: with reals)

--- py/hogparser.py
+++ py/hogparser.py
@@ -10,13 +10,16 @@ class HoGParser:
 
     _graph_pattern = re.compile('(?P<adjacency>(?:[0-9]+:[0-9 ]*\n)*)(?P<invariants>(?:[a-zA-Z- ]+:.+\n)+)')
     _adjacency_pattern = re.compile('(?P<vertex>[0-9])+:(?P<neighbors>[0-9 ]*)\n')
-    _invariant_pattern = re.compile('(?:(?P<invariant>[a-zA-Z- ]+): (?P<value>.+))\n')
+    _invariant_pattern = re.compile('(?:(?P<invariant>[a-zA-Z- ]+): (?P<value>.+))')
 
     def __init__(self, hog_struct_path):
         with open(hog_struct_path) as j:
             self._structure = json.load(j)
         last_line_start = list(self._structure)[-1]
         self._is_last_line = lambda line: line.startswith(last_line_start)
+
+    def _convert_name(self, name):
+        return (name.lower()).replace(' ', '_').replace('-', '_')
 
     def _get_preadjacency(self, neighborhoods):
         # HoG vertices start at 1
@@ -66,25 +69,43 @@ class HoGParser:
 
         inv_list = []
         for m in self._invariant_pattern.finditer(invariants):
-            inv, v = get_invariant(m)
-            inv_list.append((inv, v))
+            inv_list.append(get_invariant(m))
         return inv_list
 
-    def _graph_to_lean(self, num, buffer):
+    def _get_lean_inv(self, invariants):
+        def convert(name, value):
+            n = self._convert_name(name)
+            v = f'some ({value})'
+            if value is True:
+                v = 'some ff'
+            elif value is False:
+                v = 'some ff'
+            elif value is None or value == 'infinity':
+                v = 'none'
+            return f'    {n} := {v}'
+        return ',\n'.join(map(lambda m: convert(*m), invariants))
+
+    def _graph_to_lean(self, num, g6, buffer):
         match = self._graph_pattern.search(buffer)
         if not match:
             raise ValueError
         size, preadjacency = self._get_preadjacency(match.group('adjacency'))
         invariants = self._get_invariants(match.group('invariants'))
-        lean_str = f'def HoG{str(num).zfill(4)} : preadjacency := from_adjacency_list {str(preadjacency)}'
+        lean_str = '  { hog .\n    graph6 := "' + g6.strip() + '",\n'
+        if num != 1:
+            lean_str = ',\n' + lean_str
+        lean_str += self._get_lean_inv(invariants)
+        lean_str += '\n  }'
+        # lean_str = f'def HoG{str(num).zfill(4)} : preadjacency := from_adjacency_list {str(preadjacency)}'
         return lean_str
 
     
-    def parse(self, input_path, output_path = None):
+    def parse(self, input_path, g6_path, output_path = None):
         fh_in = open(input_path, 'r')
+        fh_g6 = open(g6_path, 'r')
         if output_path != None:
             fh_out = open(output_path, 'w')
-            fh_out.write('import hog_graph\n\n')
+            fh_out.write('import .hog\n\nnamespace hog\n\ndef database := [\n')
         
         count = 1
         buffer = ''
@@ -96,15 +117,29 @@ class HoGParser:
             # clear buffer
             if not graph_complete:
                 continue
-            lean_code = self._graph_to_lean(count, buffer)
-            print(lean_code)
+            lean_code = self._graph_to_lean(count, fh_g6.readline(), buffer)
             if output_path != None:
-                fh_out.write(lean_code + '\n')
+                fh_out.write(lean_code)
             # else:
             #     print(lean_code)
             buffer = ''
             count += 1
         
         if output_path != None:
+            fh_out.write('\n]\n\nend hog')
             fh_out.close()
         fh_in.close()
+
+    def write_lean_structure(self):
+        out = 'structure hog : Type :=\n (graph6 : string)\n'
+        for i, t in self._structure.items():
+            n = self._convert_name(i)
+            if t == 'bool':
+                out += f' ({n} : option bool)\n'
+            elif t == 'int':
+                out += f' ({n} : option nat)\n'
+            elif t == 'float':
+                out += f' ({n} : option real)\n'
+            else:
+                raise ValueError
+        print(out)
--- py/write-lean-hog-db.py
+++ py/write-lean-hog-db.py
@@ -3,7 +3,9 @@ import hogparser
 # the invariants should have the same order as in the input file
 # in particular, the last invariant is used in parsing as a terminating condition
 structure_path = 'py/hog-invariants.json'
-input_path = 'etc/order-le-6.txt'
+input_path = 'etc/order-le-6-test.txt'
+g6_path = 'etc/order-le-6-g6.txt'
 
 hog = hogparser.HoGParser(structure_path)
-hog.parse(input_path, 'src/test.lean')
+hog.write_lean_structure()
+hog.parse(input_path, g6_path, 'src/hog/db_test.lean')
