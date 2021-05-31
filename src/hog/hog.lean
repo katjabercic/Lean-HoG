c667d3ae561947f631d9dd86fe0f4f4701f0e238
Katja Berčič <katja.bercic@gmail.com>
2021-05-31 14:32:20 +0200

    Remove floats

--- src/hog/hog.lean
+++ src/hog/hog.lean
@@ -1,13 +1,10 @@
 import combinatorics.simple_graph.basic
-import data.real.basic
 import .decode
 
 -- The definition of data imported from HoG
 structure hog : Type :=
  (graph6 : string)
  (acyclic : option bool)
- (algebraic_connectivity : option real)
- (average_degree : option real)
  (bipartite : option bool)
  (chromatic_index : option nat)
  (chromatic_number : option nat)
@@ -15,7 +12,6 @@ structure hog : Type :=
  (claw_free : option bool)
  (clique_number : option nat)
  (connected : option bool)
- (density : option real)
  (diameter : option nat)
  (edge_connectivity : option nat)
  (eulerian : option bool)
@@ -23,8 +19,6 @@ structure hog : Type :=
  (girth : option nat)
  (hamiltonian : option bool)
  (independence_number : option nat)
- (index : option real)
- (laplacian_largest_eigenvalue : option real)
  (longest_induced_cycle : option nat)
  (longest_induced_path : option nat)
  (matching_number : option nat)
@@ -38,8 +32,6 @@ structure hog : Type :=
  (planar : option bool)
  (radius : option nat)
  (regular : option bool)
- (second_largest_eigenvalue : option real)
- (smallest_eigenvalue : option real)
  (vertex_connectivity : option nat)
 
 namespace hog
