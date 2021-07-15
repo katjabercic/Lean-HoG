91e21359fb39515d1827a786dd21c6ccc76c2256
Katja Berčič <katja.bercic@gmail.com>
2021-07-15 19:19:23 +0200

    Add larger graphs (up to order 22)

--- py/write-lean-hog-db.py
+++ py/write-lean-hog-db.py
@@ -3,14 +3,15 @@ import hogparser
 # the invariants should have the same order as in the input file
 # in particular, the last invariant is used in parsing as a terminating condition
 
-files = ['0-8', '9-11', '12-19']
+files = ['0-8', '9-11', '12-19', '20-21', '22']
 
 settings = {
     'structure_path': 'py/hog-invariants.json',
     'inputs': list(map(lambda x: (f'etc/hog-order-{x}-inv.txt', f'etc/hog-order-{x}-g6.txt'), files)),
     'output_path': 'src/hog/db_test.lean',
     'write_floats': False,
-    'limit': 2690
+    'limit': 0
+    # before 2D list, limit: 2690
 }
 hog = hogparser.HoGParser(settings)
 hog.write_lean_structure()
