3a7f1273efacc02c50a00a2f9cf95ace4e39e23d
Katja Berčič <katja.bercic@gmail.com>
2021-08-29 17:02:04 +0200

    Combine parser classes into one file, switch to alphabetically parsing data files in dir

--- convert/convert.py
+++ convert/convert.py
@@ -1,22 +1,7 @@
-from os import listdir
-from os.path import isfile, join
-
 import hog_parser
 
-# Input directory, relative to the project root
-srcdir = 'data'
-
-# Input files
-input_files = [
-    (f'{srcdir}/hog-order-{x}-inv.txt', f'{srcdir}/hog-order-{x}-g6.txt')
-    for x in ['0-8', '9-11', '12-19', '20-21', '22', '23', '24', '25-26', '27', '28', '29',
-              '30-regular', '30-nonregular-1', '30-nonregular-2',
-              '31-33', '34', '35-41', '42-43', '44-49', '50-99',
-              '100-149', '150-189', '190-209', '210-']
-  ]
-
 settings = {
-    'inputs': input_files,
+    'srcdir': 'data', # Input directory, relative to the project root
     'output_path': 'src/hog/data', # output directory, relative to project root
     'db_name': 'hog_data_', # prefix for output file names
     'db_main': 'hog_data', # name of the main data Lean module
