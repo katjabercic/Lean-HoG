677632b6e2edc82c3c9ff3ac6eb3e7c00538d1cd
Andrej Bauer <Andrej.Bauer@andrej.com>
2021-09-03 15:27:29 +0200

    some graph invariants

---
+++ src/hog/graph_invariant.lean
@@ -0,0 +1,26 @@
+import combinatorics.simple_graph.basic
+
+section fintype_helpers
+
+  def argmax {α : Type} [fintype α] (f : α → ℕ)
+    : { x : α | ∀ y , f y ≤ f x } :=
+    sorry
+
+end fintype_helpers
+
+section degree
+
+variables {V : Type} [fintype V] (g : simple_graph V) [decidable_rel g.adj]
+
+def nbh (x : V) := {y : V | g.adj x y}
+
+instance nbh_fintype (x : V) : fintype (nbh g x) :=
+  begin
+    sorry
+  end
+
+def degree (x : V) : ℕ := fintype.card (nbh g x)
+
+def max_degree : ℕ := degree g (argmax (degree g))
+
+end degree
