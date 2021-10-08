d0f5ebaa558e1ade5152180d4258f7e61b96766b
Andrej Bauer <Andrej.Bauer@andrej.com>
2021-10-08 15:55:44 +0200

    Roast and braise pigs

--- src/hog/pig.lean
+++ src/hog/pig.lean
@@ -1,9 +1,13 @@
-import tactic 
+import tactic
+open tactic
+open interactive (parse)
+open interactive.types (texpr)
+open lean.parser (ident)
 
 structure pig : Type :=
  (n m : ℕ)
  
-structure roast_pig : Type :=
+structure pork : Type :=
   (p : pig)
   (roasted : p.n = p.m)
   
@@ -16,17 +20,42 @@ def piki : pig :=
 def jakob : pig :=
 { n := 4,
   m := 3 }
-  
 
-def roast_piki : roast_pig :=
+def roast_piki : pork :=
 { p := piki,
   roasted := rfl }
-  
-meta def try_roasting : pig → option roast_pig :=
-λ p, if eq : p.n = p.m 
-then some { p := p, roasted := eq }
-else none
 
-#reduce try_roasting piki
-#reduce try_roasting jakob
-  
+meta def tactic.interactive.diag_tactic (e : parse texpr) : tactic unit :=
+do { t ← target, 
+     p ← i_to_expr_strict ``((%%e, %%e) : %%t), 
+     exact p
+   }
+
+def foo (i : ℕ) : ℕ × ℕ :=
+begin
+  diag_tactic (i + 3)
+end
+
+#print foo
+
+meta def tactic.interactive.roast (e : parse texpr) : tactic unit :=
+do { r ← i_to_expr_strict ``(pork.mk %%e (eq.refl (%%e).n)),
+     exact r
+   }
+
+meta def tactic.interactive.sautee (e : parse texpr) : tactic unit :=
+do { r ← i_to_expr_strict ``(pork.mk %%e (eq.refl (%%e).n)),
+     exact r
+   }
+
+meta def tactic.interactive.braise (e : parse texpr) : tactic unit :=
+do { t ← to_expr ``((%%e).n = (%%e).m),
+     (_, p) ← solve_aux t (tactic.reflexivity),
+     r ← i_to_expr_strict ``(pork.mk %%e %%p),
+     exact r
+   }
+
+def peceni_piki : pork := by roast piki
+def pohani_piki : pork := by braise piki
+
+-- def peceni_jakob : pork := by roast jakob
