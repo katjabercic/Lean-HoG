73e7792811f8571f02227f6692fc3f3842e716da
Andrej Bauer <Andrej.Bauer@andrej.com>
2021-11-17 17:19:27 +0100

    Remove experimental and unused stuff.

--- src/hog/example_data.lean
+++ src/hog/example_data.lean
@@ -1,15 +1,22 @@
 import .tactic
+import .graph
 
 namespace hog
 
-def cycle3 : simple_graph (fin 3) :=
-by from_adjacency 3 with
-  λ (i : ℕ) (j : ℕ), (match i, j with
-  | 0, 1 := tt | 1, 0 := tt
-  | 1, 2 := tt | 2, 1 := tt
-  | 2, 0 := tt | 0, 2 := tt
-  | _, _ := ff -- catch all case for false
-  end : bool)
+def cycle3 : simple_irreflexive_graph :=
+  { simple_irreflexive_graph .
+    vertex_size := 3,
+    edge :=
+      (λ (i : fin 3) (j : fin 3),
+        (match i.val, j.val with
+        | 0, 1 := tt | 1, 0 := tt
+        | 1, 2 := tt | 2, 1 := tt
+        | 2, 0 := tt | 0, 2 := tt
+        | _, _ := ff -- catch all case for false
+        end : bool))     
+  }
+
+instance: hog_edge_size cycle3 := ⟨ 3 , rfl ⟩
 
 end hog
 
