fe603d5c0b1d0bc6eacd10b4fc336e949e8d7338
Katja Berčič <katja.bercic@gmail.com>
2021-11-19 13:36:19 +0100

    Switch to templates, clean up script, document

--- convert/hog_parser.py
+++ convert/hog_parser.py
@@ -3,6 +3,34 @@ import os
 import math
 from string import Template
 
+############################################################################################
+# UTILITIES
+############################################################################################
+
+def _get_template(name):
+    fh_template = open(os.path.join('convert', f'template_{name}.txt'), 'r')
+    raw_template = fh_template.read()
+    fh_template.close()
+    return Template(raw_template), raw_template
+
+# Returns two templates:
+# everything before the `split_on` string, the preamble template, and
+# everything after the `split_on` string, the epilog template, and
+def _get_split_template(name, split_on):
+    t_raw = _get_template(name)[1]
+    splt_tmpl = t_raw.split(split_on)
+    assert len(splt_tmpl) == 2
+    return Template(splt_tmpl[0]), Template(splt_tmpl[1])
+
+def _join_templates(join_string, template, start, end, prefix='', suffix=''):
+    r = prefix
+    r += join_string.join(template(i) for i in range(start, end))
+    return r + suffix
+    
+############################################################################################
+# GRAPH CLASS
+############################################################################################
+
 class HoGGraph:
     """Holds structural information about HoG graphs. 
     Instances represent single graphs and provide a method to output Lean code."""
@@ -52,11 +80,10 @@ class HoGGraph:
     # The last line is used to check the terminating condition
     last_line_start = "Vertex Connectivity"
 
-    def __init__(self, name, g6, txt, lean_type, write_floats):
+    def __init__(self, name, g6, txt, write_floats):
         self.name = name
         self.escaped_g6 = g6.strip().replace('\\', '\\\\')
         self._write_floats = write_floats
-        self._raw_hog_type = lean_type
 
         graph_pattern = re.compile('(?P<adjacency>(?:[0-9]+:[0-9 ]*\n)*)(?P<invariants>(?:[a-zA-Z- ]+:.+\n)+)')
         match = graph_pattern.search(txt)
@@ -65,7 +92,14 @@ class HoGGraph:
         self._size, self._adjacency = self._get_size_adjacency(match.group('adjacency'))
         self._invariants = self._get_invariants(match.group('invariants'))
         
-        # INSTANCES
+        # INSTANCES DICTIONARY
+        # This gets used in the main loop to write out Lean database files for all invariants
+        # The key should be just instance_name, without the prefix
+        # self.name - name of the graph (i.e. hog007)
+        # self._invariants is a dictionary containing a type and a value for each invariant:
+        #   { 'type': inv_type, 'value': value }
+        # The keys are invariant names used in the HoG exports (also listed in HoGGraph._structure)
+        # The values are checked minimally in _get_invariants.
         self.lean_instances = {
             'edge_size': f'\ninstance: hog_edge_size {self.name} := ⟨ {self._invariants["Number of Edges"]["value"]} , rfl ⟩\n'
         }
@@ -133,21 +167,22 @@ class HoGGraph:
     def lean_graph_def(self):
         """Output a single graph as a Lean structure."""
 
-        fh_template = open(os.path.join('convert', 'template_graph.txt'), 'r')
-        raw_template = fh_template.read()
-        template = Template(raw_template)
-        fh_template.close()
-
+        template, raw_template = _get_template('graph')
         pad = len(re.search('(?P<indent> *)\$adjacency', raw_template).group('indent'))
         def arc(v1, v2):
             return f'| {v1}, {v2} := tt'
         def line(v1, v2):
             return pad*' ' + arc(v1, v2) + ' ' + arc(v2, v1)
         catch_all = pad*' ' + '| _, _ := ff -- catch all case for false'
-        adj = '\n'.join(line(*e) for e in self._adjacency) + '\n' + catch_all
+
+        adj = ('\n'.join(line(*e) for e in self._adjacency) + '\n' + catch_all).lstrip()
 
         return '\n\n' + template.substitute(name=self.name, size=self._size, adjacency=adj)
 
+
+############################################################################################
+# ITERATOR
+############################################################################################
 
 # Iterates over the given list of files with HoG exports, graph by graph
 # Returns the consecutive number of the current graph, the graph6 and the 
@@ -216,6 +251,12 @@ class HoGIterator:
         return graph, self._fh_g6.readline()
 
 
+############################################################################################
+# PARSER
+############################################################################################
+
+# The main loop
+# Writes out all the Lean files
 
 class HoGParser:
     """Converter from original HoG data to Lean code."""
@@ -225,7 +266,7 @@ class HoGParser:
             return int(math.log10(math.ceil(n))) + 1
 
         self._s = settings
-        self._hog_iterator = iter(HoGIterator(settings['srcdir'], settings['limit'], HoGGraph.last_line_start))
+        self._iterator = iter(HoGIterator(settings['srcdir'], settings['limit'], HoGGraph.last_line_start))
         self._part = 0
         max_estimate = self._s['graph_id_length']
         if self._s['limit'] > 0:
@@ -233,8 +274,6 @@ class HoGParser:
         self._graph_name_length = max_estimate
         self._part_name_length = number_of_digits(pow(10, max_estimate) / self._s['graphs_per_file'])
         self._obj_name = self._s['obj_name']
-        self._raw_hog_type = self._s['raw_hog_type']
-        self._raw_hog_namespace = self._s['raw_hog_namespace']
 
     def _ensure_output_directory(self):
         """Create the output directory if it does not exist yet."""
@@ -243,7 +282,7 @@ class HoGParser:
             print (f'Creating {d}')
             os.mkdir(d)
 
-    # Naming conventions
+    # Naming conventions, file paths
 
     def _part_number(self, n):
         """Data part number as a string with leading zeroes."""
@@ -254,70 +293,68 @@ class HoGParser:
         return self._obj_name + str(num).zfill(self._graph_name_length)
 
     def _lean_module_part(self, invariant, n):
-        """The name of the n-th Lean data module."""
+        """The name of the n-th Lean data module for an invariant."""
         return f"{self._s['db_name']}{invariant}_{self._part_number(n)}"
+
+    def _output_file_part(self, invariant):
+        """The file for the n-th Lean data module for an invariant."""
+        return os.path.join(self._s['output_path'], f"{self._lean_module_part(invariant, self._part)}.lean")
 
     def _output_file_main(self):
         """The name of main Lean data file."""
-        return os.path.join(self._s['output_path'], f"{self._s['db_main']}.lean")
-
-    def _output_file_part(self, invariant, n):
-        """The name of the n-th Lean data file."""
-        return os.path.join(self._s['output_path'], f"{self._lean_module_part(invariant, n)}.lean")
+        return os.path.join(self._s['output_path'], f"{self._s['db_name']}{self._s['db_main']}.lean")
     
-    # Templates
+    # Templates, files
 
     def _names_list(self, start, end):
-        """Prints a list of graph names from start to end (including end)"""
-        if self._s['graphs_per_file'] < 1:
-            return ''
-        r = '[' + self._graph_name(start)
-        for i in range(1, end - start + 1):
-            br = '],\n[' if ((i) % self._s['graphs_per_line']) == 0 else ', '
-            r += br + self._graph_name(start + i)
-        return r + ']'
-    
-    def _get_db_instance_preamble(self, part):
-        part = self._part_number(part)
-        return f'import ..tactic\nimport ..graph\nimport .hog_graph_{part}\n\nnamespace hog\n\n'
-    def _get_db_instance_epilog(self):
-        return '\n\nend hog'
-    
-    def _get_db_preamble(self):
-        return f'import ..tactic\nimport ..graph\n\nnamespace hog\n'
-    def _get_db_epilog(self, start, end, part):
-        identifier = 'db_' + self._part_number(part)
-        return '\n\n\ndef ' + identifier + ' := [\n' + self._names_list(start, end) + '\n]\n\nend hog'
-    
+        """Prints a list of lists of graph names from start to end (including end)"""
+        gpl = self._s['graphs_per_file']
+        if gpl < 1: return ''
+        else: return '[' + '\n'.join(
+            _join_templates(', ', self._graph_name, i, min(i + gpl, end + 1), '[', ']')
+            for i in [start + j*gpl for j in range((end-start)//gpl)]
+            ) + ']'
+
     def _write_graph_files(self, start):
         """Write a set of graph files, one file for graph definitions, one for each of the invariants"""
 
+        def pfile_open_write_preamble(invariant, preamble):
+            """Open the n-th Lean data file for graphs or invariant and write the preamble."""
+            fh = open(self._output_file_part(invariant), 'w')
+            fh.write(preamble)
+            return fh
+        def pfile_write_epilog_close(fh, epilog):
+            """Write the epilog onto then-th Lean data file for graphs or invariant and close."""
+            fh.write(epilog)
+            fh.close()
+
+        it_pre, it_epl = _get_split_template('instance', '$instances')
+        gt_pre, gt_epl = _get_split_template('db_part', '$graphs')
+
+        # Main loop
         exhausted_all_graphs = False
         had_graphs = False
         count = start - 1
         for i in range(self._s['graphs_per_file']):
             try:
-                count, g6, inv = next(self._hog_iterator)
-                graph = HoGGraph(self._graph_name(count), g6, inv, self._raw_hog_type, self._s['write_floats'])
+                count, g6, inv = next(self._iterator)
+                graph = HoGGraph(self._graph_name(count), g6, inv, self._s['write_floats'])
                 instances = graph.lean_instances
 
                 # If the output path is set, open files at the beginning
                 if i == 0 and self._s['output_path'] != None: # write beginning of file
                     had_graphs = True
-                    fh_out = open(self._output_file_part('graph', self._part), 'w')
-                    fh_out.write(self._get_db_preamble())
-                    fh_out_inst = {}
+                    fhg = pfile_open_write_preamble('graph', gt_pre.substitute())
+                    fhi = {}
                     for type_class in instances.keys():
-                        fh_out_inst[type_class] = open(self._output_file_part(type_class, self._part), 'w')
-                        fh_out_inst[type_class].write(self._get_db_instance_preamble(self._part))
+                        fhi[type_class] = pfile_open_write_preamble(type_class, it_pre.substitute(part=self._part))
 
-                print(graph.name, fh_out_inst.keys())
                 # Print the graph definition 
                 lean_graph = graph.lean_graph_def()
                 if self._s['output_path'] != None:
-                    fh_out.write(lean_graph)
+                    fhg.write(lean_graph)
                     for type_class, instance in instances.items():
-                        fh_out_inst[type_class].write(instance)
+                        fhi[type_class].write(instance)
                 else: # When not writing to files
                     print(lean_graph)
 
@@ -331,31 +368,11 @@ class HoGParser:
 
         # Close all files that need closing
         if self._s['output_path'] != None and had_graphs:
-            fh_out.write(self._get_db_epilog(start, count, self._part))
-            fh_out.close()
+            pfile_write_epilog_close(fhg, gt_epl.substitute(part=self._part, lists=self._names_list(start, count)))
             for type_class in instances.keys():
-                fh_out_inst[type_class].write(self._get_db_instance_epilog())
-                fh_out_inst[type_class].close()
+                pfile_write_epilog_close(fhi[type_class], it_epl.substitute())
         print(f'Converting graphs: {count}  ', end='\r')
         return count, exhausted_all_graphs
-
-    def _get_main_db(self, num_parts):
-        module_imports = '\n'.join([
-            f'import .{self._lean_module_part("graph", p)}'
-            for p in range(1, num_parts)
-            ])
-        module_part_names = ', '.join([
-            f'db_{self._part_number(p)}'
-            for p in range(1, num_parts)
-            ])
-        return (
-            f'import ..tactic\n'
-            f'import ..graph\n\n'
-            f'{module_imports}\n'
-            f'\n\nnamespace hog\n\ndef data := ['
-            f'{module_part_names}\n'
-            f']\n\nend hog\n'
-        )
 
     # Write all Lean module files
 
@@ -376,6 +393,11 @@ class HoGParser:
 
         # Write out the main data file
         with open(self._output_file_main(), 'w') as fh_out:
-            fh_out.write(self._get_main_db(self._part))
+            module_imports = _join_templates('\n', lambda p: f'import .{self._lean_module_part("graph", p)}', 1, self._part)
+            module_part_names = _join_templates(', ', lambda p: f'db_{self._part_number(p)}', 1, self._part)
+            template = _get_template('db_main')[0]
+            fh_out.write(template.substitute(import_graph_modules=module_imports, db_parts_list=module_part_names))
+
+        # Report on the number of graphs processed
         print(f'Total number of graphs: {count}')
  
