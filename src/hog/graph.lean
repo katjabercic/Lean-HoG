236d634fdd2acda4dbb6f758742ca759e16dcc1f
Andrej Bauer <Andrej.Bauer@andrej.com>
2021-12-10 16:53:29 +0100

    Quick-union for connected components

--- src/hog/graph.lean
+++ src/hog/graph.lean
@@ -5,7 +5,8 @@ import .tactic
 
 structure simple_irreflexive_graph : Type :=
   (vertex_size : ℕ)
-  (edge : fin vertex_size → fin vertex_size → bool)
+  (edge : fin vertex_size → fin vertex_size → Prop)
+  [edge_decidable : decidable_rel edge]
   (irreflexive : (∀ i, ¬ edge i i) . bool_reflect)
   (symmetric : (∀ i j, edge i j → edge j i) . bool_reflect)
 
@@ -14,16 +15,31 @@ structure simple_irreflexive_graph : Type :=
 def vertex (g : simple_irreflexive_graph) := fin (g.vertex_size)
 
 def edge_size (g : simple_irreflexive_graph) : ℕ :=
-  fintype.card { e : fin g.vertex_size × fin g.vertex_size | e.fst < e.snd  ∧ g.edge e.fst e.snd }
+begin
+  haveI := g.edge_decidable,
+  exact fintype.card { e : fin g.vertex_size × fin g.vertex_size | e.fst < e.snd  ∧ g.edge e.fst e.snd }
+end
 
 class hog_edge_size (g : simple_irreflexive_graph) : Type :=
   (edge_size_val : ℕ)
   (edge_size_eq : edge_size g = edge_size_val . obviously)
 
 -- add an edge to a graph
-def add_edge (g : simple_irreflexive_graph) (x y : vertex g) (ne : x ≠ y) : simple_irreflexive_graph :=
+@[reducible]
+def add_edge (g : simple_irreflexive_graph) (x y : vertex g) : simple_irreflexive_graph :=
+begin
+  haveI := g.edge_decidable,
+  exact
   { vertex_size := g.vertex_size
-  , edge := λ i j, (i, j) = (x, y) ∨ (j, i) = (x, y) ∨ g.edge i j
+  , edge := λ i j, (x ≠ y ∧ ((i, j) = (x, y) ∨ (j, i) = (x, y))) ∨ g.edge i j
   , irreflexive := begin simp [g.irreflexive]; assumption end
   , symmetric := begin simp_intros i j p, have := g.symmetric i j, tauto end
   }
+end
+
+-- adding a loop does nothing
+def add_loop (g : simple_irreflexive_graph) (x y z : vertex g) :
+  g.edge x y ↔ (add_edge g z z ).edge x y :=
+begin
+  simp
+end
