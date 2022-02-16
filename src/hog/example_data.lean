14087d07c6c3d05385a15e4955b3b31bbe657c7f
cilinder <jure.taslak@gmail.com>
2022-02-16 16:54:34 +0100

    Reconstructing connected components
    Most of the code works as before just by renaming things.
    
    Tried to redo the proof that adding an edge again gives a decomposition.
    Proved one direction, but for the other got stuck trying to prove:
    If you add an edge (x,y) and i and j are now connected, then either:
    - they were already connected or
    - i was connected with x and j was connected with y or
    - j was connected with y and i was connected with x
    
    If we can show this, the proof will follow (did one case using this onproved lemma,
    the others should be similar).
    The issue is, otherwise there is not way to relate connectedness in the new
    graph with connectedness in the original one.

--- src/hog/example_data.lean
+++ src/hog/example_data.lean
@@ -1,12 +1,19 @@
 import .tactic
 import .graph
+import .component
 
 namespace hog
 
 def cycle3 : simple_irreflexive_graph :=
   from_edge_list 3 [(0,1), (1,2), (2,0)]
 
--- instance: hog_edge_size cycle3 := ⟨ 3, rfl ⟩
+#check cycle3
+
+instance: hog_edge_size cycle3 := ⟨ 3, rfl ⟩
+
+#check max_degree cycle3
+
+#check connected cycle3 (fin.mk 0 (nat.lt_of_sub_eq_succ rfl)) (fin.mk 1 (nat.lt_of_sub_eq_succ rfl))
 
 -- instance: hog_max_degree cycle3 := ⟨ 2, rfl ⟩
 
