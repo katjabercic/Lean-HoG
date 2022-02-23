09b8a67edd5006e68154428ed4522471f5e350a7
cilinder <jure.taslak@gmail.com>
2022-02-24 00:00:30 +0100

    Tree examples

--- src/hog/example_data.lean
+++ src/hog/example_data.lean
@@ -1,7 +1,7 @@
 import .tactic
 import .graph
-import .component
 import .path
+import .tree
 
 namespace hog
 
@@ -16,19 +16,24 @@ instance: hog_edge_size cycle3 := ⟨ 3, rfl ⟩
 
 #check connected cycle3 (fin.mk 0 (nat.lt_of_sub_eq_succ rfl)) (fin.mk 1 (nat.lt_of_sub_eq_succ rfl))
 
-example : path cycle3 :=
-{ s := @fin.mk 3 0 (by obviously),
-  t := @fin.mk 3 1 (by obviously),
-  edges := let z := @fin.mk 3 0 (by obviously) in let o := @fin.mk 3 1 (by obviously) in [{i := z, j := o, H := by bool_reflect}],
-  nonempty := by sorry,
-  is_path := begin  end,
-  correct_ends := _ }
-
 -- instance: hog_max_degree cycle3 := ⟨ 2, rfl ⟩
 
 -- instance: hog_min_degree cycle3 := ⟨ 2, rfl ⟩
 
 -- instance: hog_regular cycle3 := ⟨ rfl ⟩
+
+
+def cycle3_tree : BT (lex ℕ ℕ) := BT.node (2,1) (BT.leaf (1,3)) (BT.leaf (3,4))
+
+#eval BT.is_bst cycle3_tree
+
+def G1 : BT (lex ℕ ℕ) := BT.node (1,4) (BT.node (1,3) (BT.leaf (1,2)) BT.empty) (BT.node (2,3) BT.empty (BT.leaf (3,4)))
+def G2 : BT (lex ℕ ℕ) := BT.node (1, 4) (BT.node (1, 3) (BT.leaf (1, 2)) (BT.empty)) (BT.node (3, 4) (BT.leaf (2, 3)) (BT.empty))
+
+def G1_bst : BST (lex ℕ ℕ) :=
+{ tree := BT.node (1,4) (BT.node (1,3) (BT.leaf (1,2)) BT.empty) (BT.node (2,3) BT.empty (BT.leaf (3,4))),
+  is_bst := by bool_reflect
+}
 
 end hog
 
