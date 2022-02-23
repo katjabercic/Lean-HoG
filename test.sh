bb2f200de1aeb380c80cae28e57063fb90771cbf
cilinder <jure.taslak@gmail.com>
2022-02-23 13:22:23 +0100

    Performance test for defining BTS from edge lists

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 t=$(date "+%y-%m-%d-%H-%M")
+touch "test_log_$t"
 for i in {1..170}
 do
 	echo $i
-	make clean
+	# make clean
 	make convert LIMIT=1 SKIP=$(($i * 100))
-	cat /home/jure/OneDrive/faks/lean/Lean-HoG/src/hog/data/hog.csv >> "test_log_$t"
-	/home/jure/Documents/process_io/bin/proccess_io "lean --make --memory=25000 src/hog/data" >> "test_log_$t"
-	stat --printf="Graph size: %s\n" "/home/jure/OneDrive/faks/lean/Lean-HoG/src/hog/data/hog_graph_00001.olean" >> "test_log_$t"
-	stat --printf="Edge size: %s\n" /home/jure/OneDrive/faks/lean/Lean-HoG/src/hog/data/hog_edge_size_00001.olean >> "test_log_$t"
+	# cat /home/jure/OneDrive/faks/lean/Lean-HoG/src/hog/data/hog.csv >> "test_log_$t"
+	/usr/bin/time -o "test_log_$t" -a lean --make --memory=25000 src/hog/data
+	num=$(printf "%05d" $(( $i * 100 + 1)))
+	stat --printf="olean size: %s\n" "/home/jure/OneDrive/faks/lean/Lean-HoG/src/hog/data/hog_$num.olean" >> "test_log_$t"
+	echo "" >> "test_log_$t"
+	# stat --printf="Edge size: %s\n" /home/jure/OneDrive/faks/lean/Lean-HoG/src/hog/data/hog_edge_size_00001.olean >> "test_log_$t"
 done
