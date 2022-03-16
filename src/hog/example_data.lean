277cf747455dccdfac58da62c03d1481f1d9a53c
cilinder <jure.taslak@gmail.com>
2022-03-16 10:38:16 +0100

    Example of a witness for components
    A manually constructed example for the graph hog2, that shows it
    has 1 connected component.
    We define the relvant functions (c, h, root, next) by hand, by cases.
    
    It was quite a pain to construct and `bool_reflect` didn't work in
    certain places as one would hope, so I had to define helper lemmas
    to actually compute the edge function between certain vertices.
    
    The issue seems like it had something to do with the decidability
    of the edge function.

--- src/hog/example_data.lean
+++ src/hog/example_data.lean
@@ -1,7 +1,9 @@
 import .tactic
 import .graph
 import .path
+import .component
 import .tree_representation
+import .tactic
 
 namespace hog
 
@@ -14,26 +16,125 @@ instance: hog_edge_size cycle3 := ⟨ 3, rfl ⟩
 
 #check max_degree cycle3
 
-#check connected cycle3 (fin.mk 0 (nat.lt_of_sub_eq_succ rfl)) (fin.mk 1 (nat.lt_of_sub_eq_succ rfl))
 
--- instance: hog_max_degree cycle3 := ⟨ 2, rfl ⟩
-
--- instance: hog_min_degree cycle3 := ⟨ 2, rfl ⟩
-
--- instance: hog_regular cycle3 := ⟨ rfl ⟩
-
-
-def cycle3_tree : BT (lex ℕ ℕ) := BT.node (2,1) (BT.leaf (1,3)) (BT.leaf (3,4))
-
-#eval BT.is_bst cycle3_tree
-
-def G1 : BT (lex ℕ ℕ) := BT.node (1,4) (BT.node (1,3) (BT.leaf (1,2)) BT.empty) (BT.node (2,3) BT.empty (BT.leaf (3,4)))
-def G2 : BT (lex ℕ ℕ) := BT.node (1, 4) (BT.node (1, 3) (BT.leaf (1, 2)) (BT.empty)) (BT.node (3, 4) (BT.leaf (2, 3)) (BT.empty))
-
-def G1_bst : BST (lex ℕ ℕ) :=
-{ tree := BT.node (1,4) (BT.node (1,3) (BT.leaf (1,2)) BT.empty) (BT.node (2,3) BT.empty (BT.leaf (3,4))),
-  is_bst := by bool_reflect
+def hog2_BST : BST Edge :=
+{ tree := BT.node {edge := (2, 3)} (BT.node {edge := (1, 4)} (BT.leaf {edge := (0, 4)}) (BT.empty)) (BT.leaf {edge := (3, 4)}),
+  is_bst := begin simp, bool_reflect end
 }
+
+def hog2 : simple_irreflexive_graph := from_BST 5 hog2_BST
+
+#eval hog2.vertex_size
+
+set_option trace.eqn_compiler.elim_match true
+
+lemma edge_4_1 : @decidable.to_bool 
+  (hog2.edge (fin.mk 4 (by bool_reflect)) (fin.mk 1 (by bool_reflect))) 
+  (hog2.edge_decidable (fin.mk 4 (by bool_reflect)) (fin.mk 1 (by bool_reflect))) 
+  = tt := by refl
+
+lemma edge_3_2 : @decidable.to_bool 
+  (hog2.edge (fin.mk 3 (by bool_reflect)) (fin.mk 2 (by bool_reflect))) 
+  (hog2.edge_decidable (fin.mk 3 (by bool_reflect)) (fin.mk 2 (by bool_reflect))) 
+  = tt := by refl
+
+lemma edge_4_3 : @decidable.to_bool 
+  (hog2.edge (fin.mk 4 (by bool_reflect)) (fin.mk 3 (by bool_reflect))) 
+  (hog2.edge_decidable (fin.mk 4 (by bool_reflect)) (fin.mk 3 (by bool_reflect))) 
+  = tt := by refl
+
+lemma edge_0_4 : @decidable.to_bool 
+  (hog2.edge (fin.mk 0 (by bool_reflect)) (fin.mk 4 (by bool_reflect))) 
+  (hog2.edge_decidable (fin.mk 0 (by bool_reflect)) (fin.mk 4 (by bool_reflect))) 
+  = tt := by refl
+
+def witness : num_components_witness :=
+let H : hog2.vertex_size = 5 := by bool_reflect in
+{ G := hog2,
+  num_components := 1,
+  c := λ v, 0,
+  h := λ v : fin hog2.vertex_size, 
+    match v with
+    | ⟨ 0, _ ⟩ := 0
+    | ⟨ 1, _ ⟩ := 4
+    | ⟨ 2, _ ⟩ := 3
+    | ⟨ 3, _ ⟩ := 2
+    | ⟨ 4, _ ⟩ := 1
+    | ⟨ _ , cond ⟩ := 0
+    end,
+  has_enough := sorry,
+  connect_edges := λ u v h, begin bool_reflect end,
+  root := λ i : fin 1, match i with 
+                      | ⟨ 0, _ ⟩ := ⟨ 0, by bool_reflect ⟩
+                      | _ := ⟨ 0, by bool_reflect ⟩ 
+                      end,
+  is_root := λ i,
+    match i with
+    | ⟨ 0, _ ⟩ := ⟨ begin bool_reflect end, begin bool_reflect end ⟩
+    | _ := ⟨ sorry, sorry ⟩
+    end,
+  uniqueness_of_roots := λ v : fin hog2.vertex_size, 
+    match v with
+    | ⟨ 0, _ ⟩ := λ h, begin bool_reflect end
+    | ⟨ 1, _ ⟩ := λ h, begin contradiction end
+    | ⟨ 2, _ ⟩ := λ h, begin contradiction end
+    | ⟨ 3, _ ⟩ := λ h, begin contradiction end
+    | ⟨ 4, _ ⟩ := λ h, begin contradiction end
+    | ⟨ _ , cond ⟩ := sorry
+    end,
+  next := λ v : fin hog2.vertex_size,
+    match v with
+    | ⟨ 0, _ ⟩ := ⟨ 0, by bool_reflect ⟩
+    | ⟨ 1, _ ⟩ := ⟨ 4, by bool_reflect ⟩
+    | ⟨ 2, _ ⟩ := ⟨ 3, by bool_reflect ⟩
+    | ⟨ 3, _ ⟩ := ⟨ 4, by bool_reflect ⟩
+    | ⟨ 4, _ ⟩ := ⟨ 0, by bool_reflect ⟩
+    | _ := ⟨ 0, by bool_reflect ⟩
+    end,
+  height_cond := λ (v : fin hog2.vertex_size) hyp,
+    match v with
+    | ⟨ 0, cond ⟩ := 
+      begin 
+        by_contra, 
+        have H : witness._match_1 ⟨0, cond⟩ = 0, 
+        bool_reflect,
+        sorry
+      end
+    | ⟨ 1, cond ⟩ :=
+      begin
+        split,
+        apply (to_bool_iff (hog2.edge (fin.mk 4 _) (fin.mk 1 _))).mp,
+        exact edge_4_1,
+        bool_reflect
+      end
+    | ⟨ 2, _ ⟩ :=
+      begin
+        split,
+        apply (to_bool_iff (hog2.edge (fin.mk 3 _) (fin.mk 2 _))).mp,
+        exact edge_3_2,
+        bool_reflect
+      end 
+    | ⟨ 3, _ ⟩ :=
+      begin
+        split,
+        apply (to_bool_iff (hog2.edge (fin.mk 4 _) (fin.mk 3 _))).mp,
+        exact edge_4_3,
+        bool_reflect
+      end 
+    | ⟨ 4, _ ⟩ :=
+      begin
+        split,
+        apply (to_bool_iff (hog2.edge (fin.mk 0 _) (fin.mk 4 _))).mp,
+        exact edge_0_4,
+        bool_reflect
+      end 
+    | _ := sorry
+    end,
+}
+
+def hog2_components_witness : number_of_connected_components := witness_components witness
+
+#check hog2_components_witness
 
 end hog
 
