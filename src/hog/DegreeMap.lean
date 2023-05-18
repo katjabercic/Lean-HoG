5b30eb190b25217b7ba8462bb5c8478f06cbc684
Andrej Bauer <Andrej.Bauer@andrej.com>
2023-05-18 15:50:36 +0200

    Use coercions for degree and nbh maps

--- src/hog/DegreeMap.lean
+++ src/hog/DegreeMap.lean
@@ -7,17 +7,20 @@ class DegreeMap (G : Graph) : Type where
   val : G.vertex → Nat
   correct : ∀ (u : G.vertex), val u = G.degree u
 
+instance {G : Graph} : CoeFun (DegreeMap G) (fun _ => G.vertex → Nat) where
+  coe := @DegreeMap.val G
+
 -- specialized constructor for JSON decoding
 def DegreeMap.mk'
   (G : Graph)
   (nbh : NeighborhoodMap G)
   (d : G.vertex → Nat)
-  (H : ∀ (u : G.vertex), d u = (nbh.val u).size) :
+  (H : ∀ (u : G.vertex), d u = (nbh u).size) :
   DegreeMap G :=
   { val := d,
     correct := by
       intro u
-      apply Eq.trans (b := (nbh.val u).size)
+      apply Eq.trans (b := (nbh u).size)
       · exact (H u)
       · apply Eq.symm ; apply nbh.neighborhoodSize
   }
