0e482b808671217f44db244da9e631de1e08cb1a
Andrej Bauer <Andrej.Bauer@andrej.com>
2023-05-18 11:29:42 +0200

    Degree map

--- src/hog/DegreeMap.lean
+++ src/hog/DegreeMap.lean
@@ -10,16 +10,16 @@ class DegreeMap (G : Graph) : Type where
 -- specialized constructor for JSON decoding
 def DegreeMap.mk'
   (G : Graph)
-  [N : NeighborhoodMap G]
+  (nbh : NeighborhoodMap G)
   (d : G.vertex → Nat)
-  (H : (decide (∀ (u : G.vertex), d u = (N.val u).size) = true)) :
+  (H : (decide (∀ (u : G.vertex), d u = (nbh.val u).size) = true)) :
   DegreeMap G :=
   { val := d,
     correct := by
       intro u
-      apply Eq.trans (b := (N.val u).size)
+      apply Eq.trans (b := (nbh.val u).size)
       · exact (of_decide_eq_true H u)
-      · apply Eq.symm ; unfold Graph.degree ; have X := (N.val u).size_is_card
+      · apply Eq.symm ; apply nbh.neighborhoodSize
   }
 
 end HoG
--- src/hog/NeighborhoodMap.lean
+++ src/hog/NeighborhoodMap.lean
@@ -10,13 +10,14 @@ class NeighborhoodMap (G : Graph) : Type where
 @[reducible, simp]
 def Graph.neighborhoodTree (G : Graph) [n : NeighborhoodMap G] (v : G.vertex) := n.val v
 
-lemma Graph.neighborhoodSize (G : Graph) [n : NeighborhoodMap G] (v : G.vertex) :
+lemma NeighborhoodMap.neighborhoodSize (G : Graph) [n : NeighborhoodMap G] (v : G.vertex) :
   G.degree v = (G.neighborhoodTree v).size := by
-  unfold degree
-
-
-
-
+  apply Eq.trans (b := Fintype.card (n.val v).set)
+  · apply Fintype.card_congr
+    apply (Equiv.subtypeEquiv (Equiv.refl G.vertex))
+    intro u
+    exact (n.correct v u)
+  · apply STree.size_is_card
 
 -- specialized constructor for JSON decoding
 def NeighborhoodMap.mk'
