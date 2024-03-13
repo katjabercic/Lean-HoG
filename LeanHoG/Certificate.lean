885b3ab07bf1a691b2ac04e8cbdc6a3c566da941
Gauvain Devillez <gauvain.devillez@gmail.com>
2024-03-13 11:27:49 +0100

    Replaced SetTree and MapTree with RBSet and RBMap

--- LeanHoG/Certificate.lean
+++ LeanHoG/Certificate.lean
@@ -4,6 +4,7 @@ import LeanHoG.SetTree
 import LeanHoG.Edge
 import LeanHoG.Graph
 import LeanHoG.Connectivity
+import LeanHoG.RBSetUtils
 
 namespace LeanHoG
 
@@ -95,20 +96,22 @@ def finOfData (n : Q(Nat)) (k : Nat) : Q(Fin $n) :=
 /-- Construct a quoted graph from graph data -/
 def graphOfData (D : GraphData) : Q(Graph) :=
   have vertexSize : Q(Nat) := Lean.mkRawNatLit D.vertexSize
-  have edges : Q(SetTree (Edge $vertexSize)) := setTreeOfArray (D.edges.map (edgeOfData vertexSize))
-  have edgeCorrect : Q(SetTree.isCorrect $(edges) = true) := (q(Eq.refl true) : Lean.Expr)
-  q(Graph.mk $vertexSize $edges $edgeCorrect)
+  have arrQ : Array Q(LeanHoG.Edge $vertexSize) := D.edges.map (LeanHoG.edgeOfData vertexSize)
+  have edges : Q(Std.RBSet (LeanHoG.Edge $vertexSize) LeanHoG.Edge.linearOrder.compare) := build_RBSet arrQ q(LeanHoG.Edge.linearOrder)
+  q(Graph.mk $vertexSize $edges)
 
 /-- Construct a connectivity certificate from connectivity data -/
 def connectivityCertificateOfData (G : Q(Graph)) (C : ConnectivityData) : Q(ConnectivityCertificate $G) :=
   have n : Q(Nat) := q(Graph.vertexSize $G)
   have root : Q(Graph.vertex $G) := finOfData n C.root
-  have nextMap : Q(MapTree (Graph.vertex $G) (Graph.vertex $G)) :=
-    mapTreeOfArray (C.next.map (fun (i,j) => (finOfData n i, finOfData n j)))
-  have next : Q(Graph.vertex $G → Graph.vertex $G) := q(fun v => MapTree.getD $nextMap v v)
-  have distToRootMap : Q(MapTree (Graph.vertex $G) Nat) :=
-    mapTreeOfArray (C.distToRoot.map (fun (i,j) => (finOfData n i, Lean.mkRawNatLit j)))
-  have distToRoot : Q(Graph.vertex $G → Nat) := q(fun v => MapTree.getD $distToRootMap 0 v)
+  have convertToPairOfFins : Nat × Nat → Q(Fin $n × Fin $n) := fun (i,j) => q(($(finOfData n i), $(finOfData n j)))
+  have nextMap : Q(Std.RBMap (Graph.vertex $G) (Graph.vertex $G) Fin.instLinearOrderFin.compare) :=
+    build_RBMap (C.next.map convertToPairOfFins) q(Fin.instLinearOrderFin)
+  have next : Q(Graph.vertex $G → Graph.vertex $G) := q(fun v => Std.RBMap.findD $nextMap v v)
+  have convertToFinNat : Nat × Nat → Q(Fin $n × Nat) := fun (i,j) => q(($(finOfData n i), $j))
+  have distToRootMap : Q(Std.RBMap (Graph.vertex $G) Nat Fin.instLinearOrderFin.compare) :=
+    build_RBMap (C.distToRoot.map convertToFinNat) q(Fin.instLinearOrderFin)
+  have distToRoot : Q(Graph.vertex $G → Nat) := q(fun v => Std.RBMap.findD $distToRootMap v 0)
   have nextAdjacent : Q(decide (∀ (v : Graph.vertex $G), v ≠ $root → Graph.adjacent v ($next v)) = true) := (q(Eq.refl true) : Lean.Expr)
   have distNext : Q(decide (∀ (v : Graph.vertex $G), v ≠ $root → $distToRoot ($next v) < $distToRoot v)) := (q(Eq.refl true) : Lean.Expr)
   q(ConnectivityCertificate.mk
@@ -121,12 +124,13 @@ def connectivityCertificateOfData (G : Q(Graph)) (C : ConnectivityData) : Q(Conn
 /-- Construct a disconnectivity certificate from connectivity data -/
 def disconnectivityCertificateOfData (G : Q(Graph)) (D : DisconnectivityData) : Q(DisconnectivityCertificate $G) :=
   have n : Q(Nat) := q(Graph.vertexSize $G)
-  have colorMap : Q(MapTree (Graph.vertex $G) (Fin 2)) :=
-    mapTreeOfArray (D.color.map (fun (i,j) => (finOfData n i, finOfData (Lean.mkRawNatLit 2) j)))
-  have color : Q(Graph.vertex $G → Fin 2) := q(fun v => MapTree.getD $colorMap 0 v)
+  have convertToFin : Nat × Nat → Q(Fin $n × Fin 2) := fun (i,j) => q(($(finOfData n i), $(finOfData (Lean.mkRawNatLit 2) j)))
+  have colorMap : Q(Std.RBMap (Graph.vertex $G) (Fin 2) Fin.instLinearOrderFin.compare) :=
+    build_RBMap (D.color.map convertToFin) q(Fin.instLinearOrderFin)
+  have color : Q(Graph.vertex $G → Fin 2) := q(fun v => Std.RBMap.findD $colorMap v 0)
   have vertex0 : Q(Graph.vertex $G) := finOfData n D.vertex0
   have vertex1 : Q(Graph.vertex $G) := finOfData n D.vertex1
-  have edgeColor : Q(SetTree.all (Graph.edgeTree $G) (fun e => $color e.fst = $color e.snd) = true) := (q(Eq.refl true) : Lean.Expr)
+  have edgeColor : Q(Std.RBSet.all (Graph.edgeTree $G) (fun e => $color e.fst = $color e.snd) = true) := (q(Eq.refl true) : Lean.Expr)
   have vertex0Color : Q($color $vertex0 = 0) := (q(Eq.refl (0 : Fin 2)) : Lean.Expr)
   have vertex1Color : Q($color $vertex1 = 1) := (q(Eq.refl (1 : Fin 2)) : Lean.Expr)
   q(DisconnectivityCertificate.mk
