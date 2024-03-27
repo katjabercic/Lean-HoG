09c2947ee7db7736c5891dd6e8a95dd02da0d8f4
Gauvain Devillez <gauvain.devillez@gmail.com>
2024-03-27 16:35:49 +0100

    2 coloring of odd cycles wip

--- LeanHoG/Invariant/Bipartiteness/Basic.lean
+++ LeanHoG/Invariant/Bipartiteness/Basic.lean
@@ -1,4 +1,5 @@
 import LeanHoG.Graph
+import LeanHoG.VertexColoring
 
 namespace LeanHoG
 
@@ -35,5 +36,39 @@ theorem Graph.is_bipartite_if_has_certificate (G : Graph) [B : BipartitenessCert
   · rw [B.vertex0Color, B.vertex1Color]
     simp
   · exact B.edgeColor
+
+
+/-
+theorem OddClosedWalkNotBipartite
+  {G : Graph}
+  (v : G.vertex)
+  (w : ClosedWalk G v) :
+  Odd w.length → ¬ Graph.isBipartite G := by
+  intros isOdd p
+  cases p with
+  | intro c H =>
+    apply Exists.elim H
+    intro a
+    intro H
+    apply Exists.elim H
+    intro b
+    intro H
+    obtain ⟨neq_col_a_b, diff_color_edges⟩ := H
+    let c_bool : G.vertex → Bool := fun u =>
+      match c u with
+      | 0 => false
+      | 1 => true
+    have c_bool_inj (u v : G.vertex) : c u ≠ c v → c_bool u ≠ c_bool v := by
+      intro ne_cu_cv
+      unfold_let c_bool
+      apply ltByCases
+    have diff_color_edges_bool : ∀ (e : G.edge), c_bool e.val.fst ≠ c_bool e.val.snd := by
+      intro e
+      simp
+      sorry
+    sorry
+    --let _ := (VertexColoring.EvenOddWalkEndpointEquality w c).left isOdd
+    --contradiction
+-/
 
 end LeanHoG
