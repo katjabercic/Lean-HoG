2dae4d6d0131d5b85a1975b407a9f403d1ee3dad
Andrej Bauer <Andrej.Bauer@andrej.com>
2024-04-05 10:30:22 +0200

    coooment & indentation

--- LeanHoG/LoadGraph.lean
+++ LeanHoG/LoadGraph.lean
@@ -42,72 +42,72 @@ unsafe def loadGraphAux (graphName : Name) (jsonData : JSONData) (tryHam : Bool)
   setReducibleAttribute graphName
   have graph : Q(Graph) := mkConst graphName []
 
-    match jsonData.connectedComponentsData? with
-    | .none => pure ()
-    | .some data =>
-      let componentsCertificateName := certificateName graphName "CertificateI"
-      let componentsCertificateQ : Q(ConnectedComponentsCertificate $graph) := connectedComponentsCertificateOfData graph data
-      Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
-        name := componentsCertificateName
-        levelParams := []
-        type := q(ConnectedComponentsCertificate $graph)
-        value := componentsCertificateQ
-        hints := .regular 0
-        safety := .safe
-      }
-      Elab.Command.liftTermElabM <| Meta.addInstance componentsCertificateName .scoped 42
+  match jsonData.connectedComponentsData? with
+  | .none => pure ()
+  | .some data =>
+    let componentsCertificateName := certificateName graphName "CertificateI"
+    let componentsCertificateQ : Q(ConnectedComponentsCertificate $graph) := connectedComponentsCertificateOfData graph data
+    Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
+      name := componentsCertificateName
+      levelParams := []
+      type := q(ConnectedComponentsCertificate $graph)
+      value := componentsCertificateQ
+      hints := .regular 0
+      safety := .safe
+    }
+    Elab.Command.liftTermElabM <| Meta.addInstance componentsCertificateName .scoped 42
 
-    match jsonData.pathData? with
-    | .none =>
-      if tryHam then
-        let g ← Elab.Command.liftTermElabM (Meta.evalExpr Graph q(Graph) graph)
-        let mbHp ← tryFindHamiltonianPath g
-        match mbHp with
-        | .none => pure ()
-        | .some hp =>
-          let data : PathData := { vertices := hp.vertices }
-          let hamiltonianPathName := certificateName graphName "hamiltonianPath"
-          let hpQ := hamiltonianPathOfData graph data
-          Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
-            name := hamiltonianPathName
-            levelParams := []
-            type := q(HamiltonianPath $graph)
-            value := hpQ
-            hints := .regular 0
-            safety := .safe
-          }
-          Elab.Command.liftTermElabM <| Meta.addInstance hamiltonianPathName .scoped 42
-      else
-        pure ()
-    | .some data =>
-      let hamiltonianPathName := certificateName graphName "hamiltonianPath"
-      let hpQ := hamiltonianPathOfData graph data
-      Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
-        name := hamiltonianPathName
-        levelParams := []
-        type := q(HamiltonianPath $graph)
-        value := hpQ
-        hints := .regular 0
-        safety := .safe
-      }
-      Elab.Command.liftTermElabM <| Meta.addInstance hamiltonianPathName .scoped 42
+  match jsonData.pathData? with
+  | .none =>
+    if tryHam then
+      let g ← Elab.Command.liftTermElabM (Meta.evalExpr Graph q(Graph) graph)
+      let mbHp ← tryFindHamiltonianPath g
+      match mbHp with
+      | .none => pure ()
+      | .some hp =>
+        let data : PathData := { vertices := hp.vertices }
+        let hamiltonianPathName := certificateName graphName "hamiltonianPath"
+        let hpQ := hamiltonianPathOfData graph data
+        Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
+          name := hamiltonianPathName
+          levelParams := []
+          type := q(HamiltonianPath $graph)
+          value := hpQ
+          hints := .regular 0
+          safety := .safe
+        }
+        Elab.Command.liftTermElabM <| Meta.addInstance hamiltonianPathName .scoped 42
+    else
+      pure ()
+  | .some data =>
+    let hamiltonianPathName := certificateName graphName "hamiltonianPath"
+    let hpQ := hamiltonianPathOfData graph data
+    Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
+      name := hamiltonianPathName
+      levelParams := []
+      type := q(HamiltonianPath $graph)
+      value := hpQ
+      hints := .regular 0
+      safety := .safe
+    }
+    Elab.Command.liftTermElabM <| Meta.addInstance hamiltonianPathName .scoped 42
 
-    match jsonData.bipartitenessData? with
-    | .none => pure ()
-    | .some data =>
-      let bipartitenessCertificateName := certificateName graphName "bipartitenessCertificateI"
-      let bipartitenessCertificateQ : Q(BipartitenessCertificate $graph) := bipartitenessCertificateOfData graph data
-      Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
-        name := bipartitenessCertificateName
-        levelParams := []
-        type := q(BipartitenessCertificate $graph)
-        value := bipartitenessCertificateQ
-        hints := .regular 0
-        safety := .safe
-      }
-      Elab.Command.liftTermElabM <| Meta.addInstance bipartitenessCertificateName .scoped 42
+  match jsonData.bipartitenessData? with
+  | .none => pure ()
+  | .some data =>
+    let bipartitenessCertificateName := certificateName graphName "bipartitenessCertificateI"
+    let bipartitenessCertificateQ : Q(BipartitenessCertificate $graph) := bipartitenessCertificateOfData graph data
+    Elab.Command.liftCoreM <| addAndCompile <| .defnDecl {
+      name := bipartitenessCertificateName
+      levelParams := []
+      type := q(BipartitenessCertificate $graph)
+      value := bipartitenessCertificateQ
+      hints := .regular 0
+      safety := .safe
+    }
+    Elab.Command.liftTermElabM <| Meta.addInstance bipartitenessCertificateName .scoped 42
 
-
+/-- Load a graph with the given Lean identifier from the given file. -/
 @[command_elab loadGraph]
 unsafe def loadGraphImpl : Elab.Command.CommandElab
   | `(load_graph $graphName $fileName try_ham ) => do
