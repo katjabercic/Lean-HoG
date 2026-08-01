1f6899e4eef01e1799ea4c162fe5a7cd1e1b2bc8
Lucy Horowitz <111657652+lucyhorowitz@users.noreply.github.com>
2026-08-01 11:47:33 +0200

    Fix DIMACS streaming stack overflow

--- LeanHoG/Util/LeanSAT.lean
+++ LeanHoG/Util/LeanSAT.lean
@@ -8,6 +8,16 @@ import Std.Tactic.BVDecide.LRAT.Parser
 
 open Trestle
 
+private partial def writeDimacsClauses
+    (stream : IO.FS.Handle) (fml : ICnf) (i : Nat) : IO Unit := do
+  if h : i < fml.size then
+    stream.putStr (Trestle.Solver.Dimacs.formatClause fml[i] ++ "\n")
+    writeDimacsClauses stream fml (i + 1)
+
+private def writeDimacs (stream : IO.FS.Handle) (fml : ICnf) : IO Unit := do
+  stream.putStr s!"p cnf {fml.maxVar} {fml.size}\n"
+  writeDimacsClauses stream fml 0
+
 def runLRATChecker (fml : ICnf) (proof : System.FilePath) : IO Bool :=
   do
     let actions ← Std.Tactic.BVDecide.LRAT.loadLRATProof proof
@@ -16,16 +26,15 @@ def runLRATChecker (fml : ICnf) (proof : System.FilePath) : IO Bool :=
     return Std.Tactic.BVDecide.LRAT.check actions stdCnf
 
 def SolverWithLRAT (solverCmd : String) (solverFlags : Array String := #[]) : Solver IO where
-  solve := fun fml => do
-    let tempFile := "proof.lrat"
+  solve := fun fml => IO.FS.withTempFile fun _ tempFile => do
     let solver ← IO.Process.spawn {
       cmd := solverCmd
-      args := solverFlags ++ #["-", tempFile]
+      args := solverFlags ++ #["-", tempFile.toString]
       stdin := .piped
       stdout := .piped
     }
     let (stdin, solver) ← solver.takeStdin
-    Solver.Dimacs.printICnf (stdin.putStr) fml
+    writeDimacs stdin fml
     stdin.flush
     let output ← IO.asTask solver.stdout.readToEnd Task.Priority.dedicated
 
@@ -33,16 +42,11 @@ def SolverWithLRAT (solverCmd : String) (solverFlags : Array String := #[]) : So
     let outputStr ← IO.ofExcept output.get
     let res ← IO.ofExcept <| Solver.Dimacs.parseResult fml.maxVar outputStr
     match res with
-    | .error =>
-      IO.FS.removeFile tempFile
-      return .error
+    | .error => return .error
     | .unsat =>
       let succeeded ← runLRATChecker fml tempFile
-      IO.FS.removeFile tempFile
       if succeeded then
         return res
       else
         return .error
-    | .sat assn =>
-      IO.FS.removeFile tempFile
-      return (.sat assn)
+    | .sat assn => return (.sat assn)
